671acf9c59209fb24f5feb8ec1426fcc3c54b161
Alive Coder <alivecoder0@gmail.com>
2022-05-15 19:39:34 +0545

    first commit

---
+++ WebApplication2/Services/SendMail.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+
+namespace WebApplication2
+{
+    public class SendMail : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            string fromMail = "your email";
+            string fromPassword = "[your email app password]";
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = subject;
+            message.To.Add(new MailAddress(email));
+            message.Body = "<html><body> " + htmlMessage + " </body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+            await smtpClient.SendMailAsync(message);
+        }
+    }
+}
